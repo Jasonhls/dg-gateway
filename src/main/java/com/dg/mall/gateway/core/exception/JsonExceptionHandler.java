6b4665fbb502d29720d205076e0ceccca32b378f
王强 <wangqiang@dg-mall.com>
2019-07-24 16:12:48 +0800

    修改配置

--- src/main/java/com/dg/mall/gateway/core/exception/JsonExceptionHandler.java
+++ src/main/java/com/dg/mall/gateway/core/exception/JsonExceptionHandler.java
@@ -121,9 +121,13 @@ public class JsonExceptionHandler implements ErrorWebExceptionHandler {
      */
     protected Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
         Map<String, Object> result = exceptionHandlerResult.get();
-        return ServerResponse.status((HttpStatus) result.get("httpStatus"))
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
+
+        return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).contentType(MediaType.APPLICATION_JSON_UTF8)
                 .body(BodyInserters.fromObject(result.get("body")));
+
+//        return ServerResponse.status((HttpStatus) result.get("httpStatus"))
+//                .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .body(BodyInserters.fromObject(result.get("body")));
     }
 
     /**
